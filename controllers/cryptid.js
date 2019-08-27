5007aa28e1e9f3b673e63a8e13a7ee40acb468b9
madmonkeyaudio-js <jonathangross@Jonathans-MBP.hsd1.wa.comcast.net>
2019-08-26 21:03:45 -0700

    cryptid hw

--- controllers/cryptid.js
+++ controllers/cryptid.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
+//GET ALL cryptids
+//This is for mysite.com/cryptids/
 router.get('/', (req, res) => {
     //GET DATA FROM FILE
     const cryptids = fs.readFileSync(__dirname + '/../cryptids.json');
     //PARSE the json object from that data
     const cryptidData = JSON.parse(cryptids);
+    //RENDER a template
     res.render('cryptids/index.ejs', {
         myCryptids: cryptidData
     });
 })
 
+//MAKE A NEW CRYPTID
 router.post('/', (req, res) => {
     // console.log(req.body);
     let cryptids = fs.readFileSync(__dirname + '/../cryptids.json')
@@ -22,6 +27,7 @@ router.post('/', (req, res) => {
 })
 
 router.get('/new', (req, res) => {
+
     res.render('cryptids/new')
 })
 
@@ -29,10 +35,58 @@ router.get('/:idx', (req, res) => {
     const cryptids = fs.readFileSync(__dirname + '/../cryptids.json');
     const cryptidData = JSON.parse(cryptids);
     //get the idx value from the url parameters
-    let cryptidIndex = parseInt(req.params.idx);
+    const index = req.params.idx || 0;
+
+    const cryptid = cryptidData[index];
+
     res.render('cryptids/show', {
-        myCryptid: cryptidData[cryptidIndex]
+        myCryptid: cryptid
     })
+    
+});
+
+//DELETE route that removes a cryptid from the DB
+router.delete('/:id', (req, res) => {
+    const cryptidObjs = fs.readFileSync(__dirname + '/../cryptids.json');
+    const cryptidJSON = JSON.parse(cryptidObjs);
+
+    let idx = req.params.idx;
+    //SPLICE OUT that one cryptid
+    cryptidJSON.splice(idx, 1);
+
+    //WRITE back to fs
+    fs.writeFileSync(__dirname + '/../cryptids.json', JSON.stringify(cryptidJSON));
+    //redirect to the same page, to hit the GET all route
+    res.redirect('/cryptids');
+})
+// GET the edit form for a particular cryptid
+router.get('/:id/edit', (req,res) => {
+    const cryptidObjs = fs.readFileSync(__dirname + '/../cryptids.json');
+    const cryptidJSON = JSON.parse(cryptidObjs);
+
+    let cryptid = cryptidJSON[req.params.id];
+
+    res.render('cryptids/edit', {
+        cryptidId: req.params.id,
+        cryptid: cryptid
+    });
+
+})
+
+//UPDATE route that mutates one of the items in DB
+router.put('/:idx', (req, res) => {
+    const cryptidObjs = fs.readFileSync(__dirname + '/../cryptids.json');
+    const cryptidJSON = JSON.parse(cryptidObjs);
+    //get the particular cryptid we want to edit
+    //then change the data inside it to the contents of the form input
+
+    let theCryptidWeWant = cryptidJSON[req.params.idx];
+
+    theCryptidWeWant.name = req.body.name;
+    theCryptidWeWant.img_url = req.body.img_url;
+
+    fs.writeFileSync(__dirname + '/../cryptids.json', JSON.stringify(cryptidJSON));
+    res.redirect('/cryptids');
 })
 
 
--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const layouts = require('express-ejs-layouts')
 const app = express();
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: false}));
+//Method Override middleware intercepts incoming POST requests
+//and if they have _method=PUT or _method=DELETE in the query string
+//It'll handle it as the respective HTTP verb
+app.use(methodOverride('_method'))
 
+//USE LAYOUTS
 app.use(layouts);
 app.use('/', express.static('static'));
 
+//USE CONTROLLERS
 app.use('/dinosaurs', require('./controllers/dinosaur'));
 app.use('/cryptids', require('./controllers/cryptid'));
 
-
+//THE ONLY PAGE NOT USING A CONTROLLER
 app.get('/', (req, res) => {
     res.render('home.ejs');
 })
 
+//THE 404 PAGE IN CASE SOMETHING GETS TYPED IN WRONG!!
 app.get('*', (req, res) => {
     res.render('404.ejs');
 })
 
-app.listen(8000, () => {
-    console.log('The server is live @ 8000!')
+//THE SERVER IS LISTENING
+app.listen(8001, () => {
+    console.log('The server is live @ 8001!')
 });
